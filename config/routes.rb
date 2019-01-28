9310f25b014acafed0fcddd0d09f325f2a0e22ea
thom.wingate <thom.wingate@gmail.com>
2019-01-27 21:29:46 -0500

    5.1 Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
+    
+  get 'simple_pages/landing_page'
 
-  root 'simple_pages#landing_page'
+  root 'simple_pages#about'
   #sets homepage for app
 
   get 'simple_pages/index'
