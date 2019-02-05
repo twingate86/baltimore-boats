cb02fc27c641e13665e5dc8001ca07e2b8912a1a
thom.wingate <thom.wingate@gmail.com>
2019-02-04 21:37:56 -0500

    testing SendGrid

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,4 +4,13 @@ class SimplePagesController < ApplicationController
   end
   def index
   end
+  def thank_you
+      @name = params[:name]
+      @email = params[:email]
+      @message = params[:message]
+      ActionMailer::Base.mail(from: @email,
+      to: 'thom.wingate@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
+  post 'simple_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
