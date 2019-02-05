f67393b127f6081085088d7c08d9b46d2762b502
thom.wingate <thom.wingate@gmail.com>
2019-02-04 22:48:54 -0500

    5.3 Sending Emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'thom.wingate@gmail.com'
   layout 'mailer'
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ApplicationMailer
+      default from: "thom.wingate@gmail.com"
+    
+       def contact_form(email, name, message)
+       @message = message
+       mail(from: email,
+         to: 'thom.wingate@gmail.com',
+         subject: "A new contact form message from #{name}")
+  end
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+end
+end
