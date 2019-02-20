d754beb850507a0f2b984debca5bc88faff314a7
thom.wingate <thom.wingate@gmail.com>
2019-02-20 12:54:10 -0500

    5.11 requested updates

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,30 @@
 class CommentsController < ApplicationController
     def create
-      @product = Product.find(params[:product_id])
-      @comment = @product.comments.new(comment_params)
-      @comment.user = current_user
+        @product = Product.find(params[:product_id])
+        @comment = @product.comments.new(comment_params)
+        @comment.user = current_user
 
-      respond_to do |format|
-        if @comment.save
-          format.html { redirect_to @product, notice: 'Review was created successfully.' }
-          format.json { render :show, status: :created, location: @product }
-        else
-          format.html { redirect_to @product, alert: 'Review was not saved successfully. Please leave both a comment and a rating!' }
-          format.json { render json: @comment.errors, status: :unprocessable_entity }
+        respond_to do |format|
+            if @comment.save
+                format.html { redirect_to @product, notice: 'Review was created successfully.' }
+                format.json { render :show, status: :created, location: @product }
+            else
+                format.html { redirect_to @product, alert: 'Review was not saved successfully. Please leave both a comment and a rating!' }
+                format.json { render json: @comment.errors, status: :unprocessable_entity }
+            end
         end
-      end
     end   
     def destroy
-          @comment = Comment.find(params[:id])
-          product = @comment.product
-          @comment.destroy
-          redirect_to product
+        @comment = Comment.find(params[:id])
+        product = @comment.product
+        @comment.destroy
+        redirect_to product
     end
 
-private
-          
-     def comment_params
+    private
+
+    def comment_params
         params.require(:comment).permit(:user_id, :body, :rating)
     end
 end
-    
+
--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,14 +1,13 @@
 class SimplePagesController < ApplicationController
-  def landing_page
-      @products = Product.limit(4)
-  end
-  def index
-  end
-  def thank_you
-      @name = params[:name]
-      @email = params[:email]
-      @message = params[:message]
-      UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
-    
+    def landing_page
+        @products = Product.limit(4)
+    end
+    def index
+    end
+    def thank_you
+        @name = params[:name]
+        @email = params[:email]
+        @message = params[:message]
+        UserMailer.contact_form(@email, @name, @message).deliver_now
+    end
 end
