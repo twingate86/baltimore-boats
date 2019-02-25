071df36916dbcacfb32e60b1c9f948785dd0562d
thom.wingate <thom.wingate@gmail.com>
2019-02-24 20:48:52 -0500

    6.4 tests all pass

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,14 +14,17 @@ describe UsersController, type: :controller do
                    expect(assigns(:user)).to eq user 
                 end
             end
-            context 'when a user is not logged in' do
-                it "redirects to login" do
+            context 'when a user is not login' do
+                it "redirects to root" do
                     get :show, params: { id: user.id }
                     expect(response).to redirect_to(new_user_session_path)
                 end
             end
             context 'when a user is logged in' do
-                it "redirects to login" do
+                before do
+                    sign_in user
+                end
+                it "redirects to root" do
                     get :show, params: { id: user2.id }
                     expect(response).to redirect_to(root_path)
                 end
