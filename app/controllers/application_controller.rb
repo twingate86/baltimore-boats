421ac84eecb8f964bd72662d0a151d4bf21b76a4
thom.wingate <thom.wingate@gmail.com>
2019-03-01 22:28:02 -0500

    6.6 Createing Payments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
     rescue_from CanCan::AccessDenied do |exception|
         redirect_to main_app.root_url, alert: exception.message
     end
+    protected  
+    def after_sign_in_path_for(resource)
+      sign_in_url = new_user_session_url
+      if request.referer == sign_in_url
+        super
+      else
+        stored_location_for(resource) || request.referer || root_path
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
 
   post 'simple_pages/thank_you'
 
+  post 'payments/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
