d5e88cddb6075692ad63cced9cf796721d2745b5
thom.wingate <thom.wingate@gmail.com>
2019-03-08 20:30:30 -0500

    6.11 Not working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
     
   #  so any dynamic links that show up inside emails from your development environment generate the right URLs 4.5    
   config.action_mailer.default_url_options = { host: 'baltimore-boats.herokuapp.com' }
+    
+  config.cache_store = :mem_cache_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2,
+                     :down_retry_delay => 60
+                    }
 end
