aba9ee4d3238ed8f0bf1298e058d92f01b6f9943
thom.wingate <thom.wingate@gmail.com>
2019-03-08 23:55:55 -0500

    6.13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,6 @@ Rails.application.configure do
                      :socket_failure_delay => 0.2,
                      :down_retry_delay => 60
                     }
+    config.web_socket_server_url = "wss://baltimore-boats.herokuapp.com/cable"
+    config.action_cable.allowed_request_origins = ['https://baltimore-boats.herokuapp.com', 'http://baltimore-boats.herokuapp.com']
 end
